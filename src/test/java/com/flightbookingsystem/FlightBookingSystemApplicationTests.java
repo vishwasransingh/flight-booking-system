b1e6395f9587a2b3507a015a936b123615c18260
vishwasransing <vishwasransing@gmail.com>
2024-01-22 11:42:38 +0530

    Initial commit

---
+++ src/test/java/com/flightbookingsystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
